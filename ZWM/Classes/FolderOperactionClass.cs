e64eafd12b4e555e583e1cbaf765a85b83d3b11e
Sebastian Jarząbek <sebastian.jarzabek@outlook.com>
2019-05-28 21:06:42 +0200

    delete copy folder

--- ZWM/Classes/FolderOperactionClass.cs
+++ ZWM/Classes/FolderOperactionClass.cs
@@ -37,27 +37,8 @@ namespace ZWM.Classes
 
         public void IfBaseFolderDontExistCopyIt()
         {
-            CopyFolder(CurrentDirectory, desktopPatch);
-            #endregion
+
         }
-        static public void CopyFolder(string sourceFolder, string destFolder)
-        {
-            if (!Directory.Exists(baseFolderName))
-                Directory.CreateDirectory(destFolder);
-            string[] files = Directory.GetFiles(sourceFolder);
-            foreach (string file in files)
-            {
-                string name = Path.GetFileName(file);
-                string dest = Path.Combine(destFolder, name);
-                File.Copy(file, dest);
-            }
-            string[] folders = Directory.GetDirectories(sourceFolder);
-            foreach (string folder in folders)
-            {
-                string name = Path.GetFileName(folder);
-                string dest = Path.Combine(destFolder, name);
-                CopyFolder(folder, dest);
-            }
-        }
+        #endregion
     }
 }
